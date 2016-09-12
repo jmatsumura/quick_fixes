2b4dc8514a416ac253ac1445ee37cc30886309e4
James Matsumura <james.matsumura@gmail.com>
2016-09-12 11:11:20 -0400

    reformatted and added extra logic

--- bsml_to_prot_locus_map.py
+++ bsml_to_prot_locus_map.py
@@ -12,7 +12,9 @@
 # 2) path to a file to essentially overwrite with the first column containing
 # protein IDs and the second column containing the protein cluster sequence
 #
-# HOW TO RUN: bsml_to_prot_locus_map.py bsml.list map.file
+# 3) path to the output file
+#
+# HOW TO RUN: bsml_to_prot_locus_map.py bsml.list map.file path_to_outfile.txt
 #
 # Note that the file generated will be found in the directory this script is called
 # from and will be called final_map.tsv
@@ -22,16 +24,19 @@
 import sys, re, gzip
 
 path_to_bsml_list = str(sys.argv[1]) # specify list_protein_clusters.cgi output to use
-path_to_map = str(sys.argv[2]) # specify map file
+path_to_map_list = str(sys.argv[2]) # specify map file (usually under muscle/*_j_ortholog_clusters/~)
+path_to_outfile = str(sys.argv[3]) # specify where the output is to go
 
 i = open(path_to_bsml_list, 'r')
-m = open(path_to_map, 'r')
-o = open('./final_map.tsv', 'w')
+m = open(path_to_map_list, 'r')
+o = open(path_to_outfile, 'w')
 
-map = {}
+pl_map = {}
 
 regex_for_locus = r'identifier="(.*)"\sid='
 regex_for_protid = r'id="(.*)"'
+regex_for_name = r'Name:\s(.*)\s+Len'
+regex_for_final_map = r'(\w+\.\w+\.\d+\.\d)\s+(.*)'
 
 locus = ''
 protid = ''
@@ -42,47 +47,68 @@ found_locus = False
 # iterate over each bsml file within the bsml list
 for line in i:
 
-    line = line.replace('\n', '')
+	line = line.replace('\n', '')
+	c = gzip.open(line, 'rb')
 
-    c = gzip.open(line, 'rb')
-
-    for x in c: # iterate over each individual  bsml file
+	for x in c: # iterate over each individual  bsml file
     
-        if 'Feature class="CDS"' in line: # locus+polypeptide is near
-            found_cds = True	
+		if 'Feature class="CDS"' in x: # locus+polypeptide is near
+			found_cds = True	
 
-        elif found_cds == True: # extract locus within CDS region
-            if 'NCBILocus' in line:
-                locus = re.search(regex_for_locus, line).group(1)
-                found_locus = True
+		elif found_cds == True and found_locus == False: # extract locus within CDS region
+			if 'NCBILocus' in x:
+				locus = re.search(regex_for_locus, x).group(1)
+				found_locus = True
 
-        # build map and reset all
-        elif 'Feature class="polypeptide"' in line and found_locus == True: 
-            protid = re.search(regex_for_protid, line).group(1)
-            map[protid] = locus
-            locus = ''
-            protid = ''
-            found_cds = False
-            found_locus = False
+		# build map and reset all
+		elif 'Feature class="polypeptide"' in x and found_locus == True: 
+			protid = re.search(regex_for_protid, x).group(1)
+			pl_map[protid] = locus
+			locus = ''
+			protid = ''
+			found_cds = False
+			found_locus = False
 
-    c.close()
+	c.close()
 
 i.close()
+
+cnt = 1
 
 # build the final desired map file
 for line in m:
 
 	line = line.replace('\n', '')
-	elements = line.split('\t')
+	c = gzip.open(line, 'rb')
+	locus_array = []
 
-	if len(elements) == 2:
-		mapped_id = elements[0] # for clarity
-        cluster_seq = elements[1]
-		mapped_locus = map[mapped_id]
-		o.write(mapped_locus + '\t' + cluster_seq  + '\n')
+	for x in c:
+		x = x.replace('\n', '')
 
-	else:
-		o.write(line + '\n')
+		if re.match(regex_for_final_map, x): # replace body protids with locus IDs
+			mapid = re.search(regex_for_final_map, x).group(1) # for clarity
+			if mapid in pl_map:
+				o.write(x.replace(mapid, pl_map[mapid]) + '\n')
+			else:
+				o.write(x + '\n')
+
+		elif 'Name:' in x: # replace header section protids with locus IDs
+			mapid = re.search(regex_for_name, x).group(1)
+			mapid = mapid.replace(" ", "") # regex shoulud have caught this, no?
+			if mapid in pl_map:
+				newLine = x.replace(mapid, pl_map[mapid])
+				locus_array.append(pl_map[mapid])
+				o.write(newLine + '\n')
+			else:
+				o.write(x + '\n')
+				print('Protein ID: ' + mapid + ' not found in BSML.')
+
+		elif x.startswith('//'): # print a line to grep for cluster members
+			o.write('cluster_' + str(cnt) + ' ' + '\t'.join(str(j) for j in locus_array) + '\n\n//\n')
+			cnt += 1
+
+		else: # maintain format of original file
+			o.write(x + '\n')
 
 m.close()
 o.close()
